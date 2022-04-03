68c4c07c424f3fea9a23b2f53168be77972d5d9f
JINYOUNGCHOI <cjy9249@tmon.co.kr>
2022-04-03 23:00:58 +0900

    feat : add file

---
+++ src/main/java/com/patten/degine_patten/DeginePattenApplication.java
@@ -0,0 +1,16 @@
+package com.patten.degine_patten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeginePattenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeginePattenApplication.class, args);
+        
+    }
+
+
+
+}
---
+++ src/test/java/com/patten/degine_patten/DeginePattenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patten.degine_patten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeginePattenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
